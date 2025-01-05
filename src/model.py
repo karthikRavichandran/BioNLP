8aa6955e1a6206c02cccd9c7f1a4bbdcabb67142
Karthik Ravichandran <tkgravikarthik@gmail.com>
2025-01-04 23:25:21 -0800

    more models

--- src/model.py
+++ src/model.py
@@ -1,14 +1,19 @@
 from vllm import LLM, SamplingParams
 from transformers import AutoTokenizer
-def init_llm():
-    model = LLM("/project/pi_hongyu_umass_edu/shared_llm_checkpoints/Meta-Llama-3.1-8B-Instruct", dtype="half")
-    tokenizer = AutoTokenizer.from_pretrained("/project/pi_hongyu_umass_edu/shared_llm_checkpoints/Meta-Llama-3.1-8B-Instruct")
+
+
+def init_llm(model="Meta-Llama-3.1-8B-Instruct"):
+    model = LLM(f"/project/pi_hongyu_umass_edu/shared_llm_checkpoints/{model}",
+                dtype="half")
+    tokenizer = AutoTokenizer.from_pretrained(
+        f"/project/pi_hongyu_umass_edu/shared_llm_checkpoints/{model}")
     messages = [{"role": "user", "content": "What is the capital of France?"}]
-    formatted_prompt =  tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+    formatted_prompt = tokenizer.apply_chat_template(messages, tokenize=False,
+                                                     add_generation_prompt=True)
     output = model.generate(formatted_prompt)
     print(output)
     return model, tokenizer
 
-if __name__=="__main__":
-    
+
+if __name__ == "__main__":
     model = init_llm()
